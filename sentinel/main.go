847eee157b9f86d53c3a7ba9876b1d3cef9d5823
John Hession <johhess@cdw.com>
2021-07-27 08:03:22 -0700

    Cleaned up Sentinel testing

--- sentinel/main.go
+++ sentinel/main.go
@@ -1,3 +1,10 @@
+/*
+	This code is meant for testing of Azure Sentinel incident creation based on rules in Log Analytics
+		-TODO: Make reusable so that sub id's etc. are not hard coded
+		-TODO: Add logic for testing all types of Sentinel rules(Fusion,ML,Scheduled, etc)
+		-TODO: Add functionality for reading YAML/JSON for Client configuration
+  */
+
 package main
 
 import (
@@ -9,38 +16,35 @@ import (
 )
 
 func main() {
+
 	rootCtx := context.Background()
+
 	var topFive int32
-	// create a VirtualNetworks client
-	//vnetClient := network.NewVirtualNetworksClient("b3c6d20c-ca4c-4ea7-af76-da58d3e316f6")
+
 	incidentClient := securityinsight.NewIncidentsClient("300f794a-d9b3-4332-a5db-b6c6d6e2a9c7")
-	//.incidentClient("b3c6d20c-ca4c-4ea7-af76-da58d3e316f6")
-	// create an authorizer from env vars or Azure Managed Service Idenity
+
 	authorizer, err := auth.NewAuthorizerFromCLI()
+
 	if err == nil {
 		incidentClient.Authorizer = authorizer
 		print("Base URI: " + incidentClient.BaseURI + "\n")
 		print("Sub ID: " + incidentClient.SubscriptionID + "\n")
 		print("UserAgent: " + incidentClient.UserAgent + "\n")
 	}
-	const topFiveCnst = 5
-	topFive = int32(topFiveCnst)
+	const testFive = 5
+
+	topFive = int32(testFive)
+
 	fmt.Printf(string(topFive))
+
 	incList, err := incidentClient.List(rootCtx, "SIEM", "sentinel-law", "", "", &topFive, "")
 	if err != nil {
 		panic(err)
 	}
-	// s := fmt.Sprintf("%v\n", incList.Values())
+
 
 	for _, v := range incList.Values() {
-		//	fmt.Printf("%v\n", v.Severity)
-		//fmt.Printf("%v\n", v.IncidentProperties)
-		// fmt.Printf("%v\n", v.UnmarshalJSON(bits))
 		fmt.Printf("%v\n", *v.Owner)
 	}
 
-	// fmt.Printf("%T\n", incList.Values())
-
-	// io.WriteString(os.Stdout, s)
-	fmt.Printf(string(topFive))
 }
